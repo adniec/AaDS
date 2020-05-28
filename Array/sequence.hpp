e7ff7ca6c21cbd0de778df530b088efe14fa2740
ethru <ethru@protonmail.com>
2020-05-28 14:18:31 +0200

    Update sequence algorithms

--- Array/sequence.hpp
+++ Array/sequence.hpp
@@ -11,3 +11,18 @@ int longestPositiveSequence(int array[], int length){
 	}
 	return longest;
 }
+
+void moveNegativeFront(int array[], int length){
+	int index = 0;
+	for(int i=0; i<length; i++)
+		if(array[i] < 0)
+			std::swap(array[index++], array[i]);
+}
+
+bool isSumOfTwoEqualNumber(int array[], int length, int number){
+	for(int i=0; i<length; i++)
+		for(int j=i+1; j<length; j++)
+			if(array[i] + array[j] == number)
+				return true;
+	return false;
+}
