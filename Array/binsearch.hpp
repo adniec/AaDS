7acd8aa9d84383f661d84951b440d22e20e0999d
ethru <ethru@protonmail.com>
2020-05-30 09:54:59 +0200

    Add recurrent binary search

--- Array/binsearch.hpp
+++ Array/binsearch.hpp
@@ -16,3 +16,13 @@ int binarySearch(int array[], int length, int element){
 	if(first < length && array[first] == element) return first;
 	return -1;
 }
+
+int binarySearchRecurrent(int array[], int first, int last, int element){
+	int mid = (first + last) / 2;
+	if(array[mid] == element) return mid;
+	if(first == last) return -1;
+	
+	if(element < array[mid])
+	 	return binarySearchRecurrent(array, first, --mid, element);
+	return binarySearchRecurrent(array, ++mid, last, element);
+}
