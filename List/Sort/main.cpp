d8b348acc8c5ec3ae2d914fdd6f7b7c5132829de
ethru <ethru@protonmail.com>
2020-06-14 17:39:27 +0200

    Add list sorting examples

---
+++ List/Sort/main.cpp
@@ -0,0 +1,45 @@
+#include "../list.hpp"
+#include "bubble.hpp"
+#include "insertion.hpp"
+#include "merge.hpp"
+#include "selection.hpp"
+#include <iostream>
+using namespace std;
+
+const int N = 10;
+
+int main(){
+    cout << "BubbleSort:";
+    cout << "\nList  : ";
+    node* bubble = get(N);
+    print(bubble);
+    cout << "Sorted: ";
+    bubbleSort(bubble, N);
+    print(bubble);
+
+    cout << "\nInsertionSort:";
+    cout << "\nList  : ";
+    node* insertion = get(N);
+    print(insertion);
+    cout << "Sorted: ";
+    insertionSort(insertion);
+    print(insertion);
+
+    cout << "\nMergeSort:";
+    cout << "\nList  : ";
+    node* merged = get(N);
+    print(merged);
+    cout << "Sorted: ";
+    mergeSort(&merged);
+    print(merged);
+
+    cout << "\nSelectionSort:";
+    cout << "\nList  : ";
+    node* selection = get(N);
+    print(selection);
+    cout << "Sorted: ";
+    selectionSort(selection);
+    print(selection);
+
+    return(0);
+}
