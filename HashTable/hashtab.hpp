a607145ac58e283ac942e4550649ccf2ef5be745
ethru <ethru@protonmail.com>
2020-06-18 17:35:49 +0200

    Add hash table structure

---
+++ HashTable/hashtab.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#define SIZE 100
+
+struct node{
+    std::string key;
+    int value;
+    node* next = nullptr;
+};
+
+unsigned int strToNum(std::string str){
+    unsigned int result = 0;
+    for(int i=0; i<str.size(); i++)
+        result ^= (int) str[i] << (i % 4) * 8;
+    return result;
+}
+
+node* search(node* hashtab[SIZE], std::string key){
+    int index = strToNum(key) % SIZE;
+    node* n = hashtab[index];
+    while(n){
+        if(n->key == key) return n;
+        n = n->next;
+    }
+    return nullptr;
+}
