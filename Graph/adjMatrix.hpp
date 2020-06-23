20ad22794c5180df638233b941714f7905ac9734
ethru <ethru@protonmail.com>
2020-06-23 13:21:34 +0200

    Add BFS for graph as matrix

--- Graph/adjMatrix.hpp
+++ Graph/adjMatrix.hpp
@@ -1,4 +1,23 @@
 #pragma once
+#include "../Queue/queue.hpp"
+
+int* breadthFirstSearch(int graph[V][V], int source){
+    int* parents = new int[V];
+    for(int i=0; i<V; i++) parents[i] = -1;
+    parents[source] = source;
+    queue q;
+    q.put(source);
+    while(!q.isEmpty()){
+        int u = q.get();
+        for(int i=0; i<V; i++){
+            if(graph[u][i] && parents[i] < 0){
+                parents[i] = u;
+                q.put(i);
+            }
+        }
+    }
+    return parents;
+}
 
 void depthFirstSearch(int graph[V][V], int source){
     visited[source] = true;
